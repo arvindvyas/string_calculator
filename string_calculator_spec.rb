c1b492b466d18b7983822f9abaedc0b427585ce9
Arvind-Vyas <arvindvyas07@gmail.com>
2024-06-02 14:25:03 +0530

    * Initializes a new instance of the StringCalculator class. * Add test for handling an empty string.

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,6 +2,14 @@ require 'rspec'
 require_relative 'string_calculator'
 
 
-describe StringCalculator do 
+describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  context "with empty string" do
+    it "returns 0" do
+      expect(calculator.add("")).to eq(0)
+    end
+  end
+
 end
 
