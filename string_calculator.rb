c0ad10e7571b66b21af33e872ca4253acfce5645
Arvind-Vyas <arvindvyas07@gmail.com>
2024-06-02 16:30:10 +0530

    Add implementation to handle Allow multiple delimiters like this: “//[delim1][delim2]\n” for example “//[*][%]\n1*2%3”

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,8 +3,9 @@ class StringCalculator
     return 0 if numbers.empty?
 
     if numbers.start_with?("//")
-      delimiter, numbers = extract_delimiter(numbers)
-      number_array = numbers.split(delimiter).map(&:to_i)
+      delimiters, numbers = extract_delimiters(numbers)
+      regex_delimiters = Regexp.union(delimiters)
+      number_array = numbers.split(regex_delimiters).map(&:to_i)
     else
       number_array = numbers.split(/,|\n/).map(&:to_i)
     end
@@ -16,15 +17,14 @@ class StringCalculator
 
 
   private
-    def extract_delimiter(numbers)
-     if numbers[2] == '['
-       delimiter = numbers[3..-1].split(']')[0]
-       numbers = numbers.split("\n", 2)[1]
-       [delimiter, numbers]
-    else
-       delimiter = numbers[2]
-       numbers = numbers[4..-1]
-       [delimiter, numbers]
+    def extract_delimiters(numbers)
+      if numbers[2] == '['
+        delimiters = numbers.scan(/\[(.*?)\]/).flatten
+        numbers = numbers.split("\n", 2)[1]
+      else
+        delimiters = [numbers[2]]
+        numbers = numbers[4..-1]
+      end
+      [delimiters, numbers]
     end
-  end
 end
