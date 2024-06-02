97b9226fb4be6fd02cf8587ecd5de2268641b13b
Arvind-Vyas <arvindvyas07@gmail.com>
2024-06-02 14:42:38 +0530

    Chage the context and also added the passing test for returns the sum of multiple numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -5,8 +5,8 @@ require_relative 'string_calculator'
 describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
-  context "with empty string" do
-    it "returns 0" do
+  context ".add" do
+    it "returns 0 for an empty string" do
       expect(calculator.add("")).to eq(0)
     end
 
@@ -16,6 +16,10 @@ describe StringCalculator do
 
     it "returns the sum of two numbers" do
       expect(calculator.add("1,5")).to eq(6)
+    end
+
+    it "returns the sum of multiple numbers" do
+      expect(calculator.add("1,2,3,4")).to eq(10)
     end
   end
 
