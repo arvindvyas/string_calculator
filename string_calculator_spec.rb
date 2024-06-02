76aee6c92b2ebe45ad838d5eb328c855fc968ed1
Arvind-Vyas <arvindvyas07@gmail.com>
2024-06-02 14:57:11 +0530

    [Failing] Add test returns the sum of numbers with different delimiters

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -26,6 +26,10 @@ describe StringCalculator do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
 
+    it "returns the sum of numbers with different delimiters" do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
+
   end
 
 end
