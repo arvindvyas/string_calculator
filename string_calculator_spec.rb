bcb5a54f6fd56f719bbb6195ef86e95cfa5c660f
Arvind-Vyas <arvindvyas07@gmail.com>
2024-06-02 14:25:04 +0530

    [Test - Failing]  returns the number for a single number

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -9,6 +9,10 @@ describe StringCalculator do
     it "returns 0" do
       expect(calculator.add("")).to eq(0)
     end
+
+    it "returns the number for a single number" do
+      expect(calculator.add("1")).to eq(1)
+    end
   end
 
 end
