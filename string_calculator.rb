08c5b2b1bec4749f95051c8065389edfad5f88ba
Arvind-Vyas <arvindvyas07@gmail.com>
2024-06-02 14:57:52 +0530

    pass test and implementation for handling different delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,13 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..-1].split("\n", 2)
+      number_array = numbers.split(delimiter).map(&:to_i)
+    else
+      number_array = numbers.split(/,|\n/).map(&:to_i)
+    end
+    number_array.sum
   end
 end
